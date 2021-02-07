ccb5768e31f3cdecda9744b804626112b690ebc3
Mike Dent <mikedent85@gmail.com>
2021-02-07 16:44:00 -0500

    endpoint tests, routes, services for posts

--- src/post/post-router.js
+++ src/post/post-router.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const PostService = require('./post-service');
 const xss = require('xss');
+const app = require('../app');
 
 const postRouter = express.Router();
 const jsonParser = express.json();
@@ -11,5 +12,77 @@ postRouter
     .get((req, res, next) => {
         PostService.getAllPosts(req.app.get('db'))
     })
+    .post(jsonParser, (req, res, next) => {
+        const { title, content } = req.body
+        const newPost = { title, content }
+
+        for (const [key, value] of Object.entries(newPost)) {
+            if (value == null) {
+                return res.status(400).json({
+                    error: { message: `Missing ${key} in request body`}
+                })
+            }
+        }
+
+        PostService.insertPost(
+            req.app.get('db'),
+            newPost
+        )
+            .then(post => {
+                res
+                .status(201)
+                .location(path.posix.join(req.originalUrl, `${post.id}`))
+                .json(post)
+            })
+            .catch(next)
+    })
+
+postRouter
+    .route('/:post_id')
+    .all((req, res, next) => {
+        PostService.getPostById(req.app.get('db'), req.params.post_id)
+            .then((post) => {
+                if (!post) {
+                    return res.status(404).json({ 
+                        error: {message: `Post doesn't exist`}
+                    });
+                };
+                res.post = post;
+                next();
+            })
+            .catch(next);
+    })
+    .get((req, res, next) => {
+        res.json({
+            id: res.post.id,
+            title: xss(res.post.title),
+            content: xss(res.post.content),
+            date_published: res.post.date_published,
+        });
+    })
+    .delete((req, res, next) => {
+        PostService.deletePost(req.app.get('db'), req.params.post_id)
+            .then(() => {
+                res.status(204).end();
+            })
+            .catch(next);
+    })
+    .patch(jsonParser, (req, res, next) => {
+        const { title, content } = req.body
+        const postToUpdate = { title, content }
+
+        const numberOfValues = Object.values(postToUpdate).filter(Boolean).length
+            if (numberOfValues === 0) {
+                return res.status(400).json({
+                    error: { message: `Request body must contain either 'title' or 'content'` }
+                })
+            }
+        
+        PostService.updatePost(req.app.get('db'), req.params.post_id, postToUpdate)
+            .then((numRowsAffected) => {
+                res.status(204).end();
+            })
+            .catch(next);
+    });
 
 module.exports = postRouter;
--- test/posts-endpoints.spec.js
+++ test/posts-endpoints.spec.js
@@ -61,147 +61,217 @@ describe("Posts endpoints", () => {
     });
   });
 
-//   describe("POST /articles", () => {
-//     it("creates an articles, responds with 201 and the new article", function() {
-//       this.retries(3);
-//       const newArticle = {
-//         title: "Test new article",
-//         style: "Listicle",
-//         content: "Test new article content..."
-//       };
-//       return supertest(app)
-//         .post("/articles")
-//         .send(newArticle)
-//         .expect(201)
-//         .expect((res) => {
-//           expect(res.body.title).to.eql(newArticle.title);
-//           expect(res.body.style).to.eql(newArticle.style);
-//           expect(res.body.content).to.eql(newArticle.content);
-//           expect(res.body).to.have.property("id");
-//           expect(res.headers.location).to.eql(`/articles/${res.body.id}`);
-//           const expected = new Date().toLocaleDateString();
-//           const actual = new Date(res.body.date_published).toLocaleDateString();
-//           expect(actual).to.eql(expected);
-//         })
-//         .then((postRes) =>
-//           supertest(app)
-//             .get(`/articles/${postRes.body.id}`)
-//             .expect(postRes.body)
-//         );
-//     });
+  describe("POST /api/posts", () => {
+    it("creates post, responds with 201 and the new post", function() {
+      this.retries(3);
+      const newPost = {
+        title: "Test New Post",
+        content: "Test new post content test..."
+      };
+      return supertest(app)
+        .post('/api/posts')
+        .send(newPost)
+        .expect(201)
+        .expect((res) => {
+          expect(res.body.title).to.eql(newPost.title);
+          expect(res.body.content).to.eql(newPost.content);
+          expect(res.body).to.have.property("id");
+          expect(res.headers.location).to.eql(`/api/posts/${res.body.id}`);
+          const expected = new Date().toLocaleDateString();
+          const actual = new Date(res.body.date_published).toLocaleDateString();
+          expect(actual).to.eql(expected);
+        })
+        .then((postRes) =>
+          supertest(app)
+            .get(`/api/posts/${postRes.body.id}`)
+            .expect(postRes.body)
+        );
+    });
 
-//     const fields = ['title', 'style', 'content'];
-//     fields.forEach(field => {
-//       const newArticle = {
-//         title: "Test new article",
-//         style: "Listicle",
-//         content: "Test new article content..."
-//       };
-//       it(`responds with 400 and an error message when ${field} field is missing`, () => {
-//         delete newArticle[field];
-//         return supertest(app)
-//           .post('/articles')
-//           .send(newArticle)
-//           .expect(400, {
-//             error: {message: `Missing ${field} in request body`}
-//           });
-//       });
-//     });
+    const fields = ['title', 'content'];
+    fields.forEach(field => {
+      const newPost = {
+        title: "Test New Post",
+        content: "Test new post content test..."
+      };
+      it(`responds with 400 and an error message when ${field} field is missing`, () => {
+        delete newPost[field];
+        return supertest(app)
+          .post('/api/posts')
+          .send(newPost)
+          .expect(400, {
+            error: {message: `Missing ${field} in request body`}
+          });
+      });
+    });
 
-//     context(`When an XSS attack article is put in, article is sanitized right away`, () => {
-//       const maliciousArticle = makeMaliciousArticle();
+    context(`When an XSS attack article is put in, article is sanitized right away`, () => {
+      const {maliciousPost, expectedPost} = makeMaliciousPost();
       
-//       it('removes XSS attack content', () => {
-//         return supertest(app)
-//           .post(`/articles`)
-//           .send(maliciousArticle)
-//           .expect(201)
-//           .expect(res => {
-//             expect(res.body.title).to.eql('Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;');
-//             expect(res.body.content).to.eql(`Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`);
-//           });
-//       });
-//     });
-//   });
+      it('removes XSS attack content', () => {
+        return supertest(app)
+          .post(`/api/posts`)
+          .send(maliciousPost)
+          .expect(201)
+          .expect(res => {
+          expect(res.body.title).to.eql(expectedPost.title)
+          expect(res.body.content).to.eql(expectedPost.content)
+          });
+      });
+    });
+  });
 
-//   describe("GET /articles/:id", () => {
-//     context(`Given no articles`, () => {
-//       it(`responds with 404`, () => {
-//         const articleId = 123456;
-//         return supertest(app)
-//           .get(`/articles/${articleId}`)
-//           .expect(404, { error: { message: `Article doesn't exist` } });
-//       });
-//     });
+  describe("GET /api/posts/:post_id", () => {
+    context(`Given no post with that ID`, () => {
+      it(`responds with 404`, () => {
+        const post_id = 123456;
+        return supertest(app)
+          .get(`/api/posts/${post_id}`)
+          .expect(404, { error: { message: `Post doesn't exist` } });
+      });
+    });
 
-//     context("Given there are articles in the database", () => {
-//       const testArticles = makeArticlesArray();
+    context("Given there are posts in the database", () => {
+      const testPosts = makePostsArray();
 
-//       beforeEach("insert articles", () => {
-//         return db.into("blogful_articles").insert(testArticles);
-//       });
+      beforeEach("insert posts", () => {
+        return db.into('posts').insert(testPosts);
+      });
 
-//       it("GET /articles/:id responds with 200 and the specified article", () => {
-//         const articleId = 3;
-//         const expected = testArticles[articleId - 1];
-//         return supertest(app)
-//           .get(`/articles/${articleId}`)
-//           .expect(200, expected);
-//       });
-//     });
+      it('GET /api/posts/:post_id responds with 200 and the specified post', () => {
+        const post_id = 3;
+        const expected = testPosts[post_id - 1];
+        return supertest(app)
+          .get(`/api/posts/${post_id}`)
+          .expect(200, expected);
+      });
+    });
 
-//     context(`Given an XSS attack article`, () => {
-//       const maliciousArticle = makeMaliciousArticle();
+    context(`Given an XSS attack post`, () => {
+      const maliciousPost = makeMaliciousPost();
       
-//       beforeEach('insert malicious article', () => {
-//         return db
-//           .into('blogful_articles')
-//           .insert([ maliciousArticle ]);
-//       });
+      beforeEach('insert malicious post', () => {
+        return db
+          .into('posts')
+          .insert([ maliciousPost ]);
+      });
       
-//       it('removes XSS attack content', () => {
-//         return supertest(app)
-//           .get(`/articles/${maliciousArticle.id}`)
-//           .expect(200)
-//           .expect(res => {
-//             expect(res.body.title).to.eql('Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;');
-//             expect(res.body.content).to.eql(`Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`);
-//           });
-//       });
-//     });
-//   });
+      it('removes XSS attack content', () => {
+        return supertest(app)
+          .get(`/api/posts/${maliciousPost.id}`)
+          .expect(200)
+          .expect(res => {
+            expect(res.body[0].title).to.eql(`Beware this malicious thing &lt;script&gt;alert("xss");&lt;/script&gt;`);
+            expect(res.body[0].content).to.eql(`Pure evil vile image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`);
+          });
+      });
+    });
+  });
 
-//   describe.only(`DELETE /articles/:article_id`, () => {
-//     context(`Given no articles`, () => {
-//       it(`responds with 404`, () => {
-//         const articleId = 123456;
-//         return supertest(app)
-//           .delete(`/articles/${articleId}`)
-//           .expect(404, { error: { message: `Article doesn't exist` } });
-//       });
-//     });
+  describe(`DELETE /api/posts/:post_id`, () => {
+    context(`Given no post with that ID`, () => {
+      it(`responds with 404`, () => {
+        const post_id = 123456;
+        return supertest(app)
+          .delete(`/api/posts/${post_id}`)
+          .expect(404, { error: { message: `Post doesn't exist` } });
+      });
+    });
 
-//     context('Given there are articles in the database', () => {
-//       const testArticles = makeArticlesArray();
+    context('Given there are posts in the database', () => {
+      const testPosts = makePostsArray();
     
-//       beforeEach('insert articles', () => {
-//         return db
-//           .into('blogful_articles')
-//           .insert(testArticles);
-//       });
+      beforeEach('insert posts', () => {
+        return db
+          .into('posts')
+          .insert(testPosts);
+      });
     
-//       it('responds with 204 and removes the article', () => {
-//         const idToRemove = 2;
-//         const expectedArticles = testArticles.filter(article => article.id !== idToRemove);
-//         return supertest(app)
-//           .delete(`/articles/${idToRemove}`)
-//           .expect(204)
-//           .then(res =>
-//             supertest(app)
-//               .get(`/articles`)
-//               .expect(expectedArticles)
-//           );
-//       });
-//     });
-//   });
-// });
+      it('responds with 204 and removes the post', () => {
+        const idToRemove = 2;
+        const expectedPosts = testPosts.filter(post => post.id !== idToRemove);
+        return supertest(app)
+          .delete(`/api/posts/${idToRemove}`)
+          .expect(204)
+          .then(res =>
+            supertest(app)
+              .get(`/api/posts`)
+              .expect(expectedPosts)
+          );
+      });
+    });
+  });
+
+  describe(`PATCH /api/posts/:post_id`, () => {
+        context('Given no post with that ID', () => {
+            it('responds with 404', () => {
+                const post_id = 123456
+                return supertest(app)
+                    .patch(`/api/posts/${post_id}`)
+                    .expect(404, {error: {message: `Post doesn't exist`}})
+            })
+        })
+
+        context('Given there are posts in the database', () => {
+            const testPosts = makePostsArray()
+
+            beforeEach('insert posts', () => {
+                return db
+                    .into('posts')
+                    .insert(testPosts)
+            })
+
+            it('responds with 204 and updates the post', () => {
+                const idToUpdate = 2
+                const updatePost = {
+                    title: 'Updated Post',
+                    content: 'Post content to update...',
+                }
+                const expectedPost = {
+                    ...testPosts[idToUpdate -1],
+                    ...updatePost
+                }
+                return supertest(app)
+                    .patch(`/api/posts/${idToUpdate}`)
+                    .send(updatePost)
+                    .expect(204)
+                    .then(res => 
+                        supertest(app)
+                            .get(`/api/posts/${idToUpdate}`)
+                            .expect(expectedPost)
+                    )
+            })
+
+            it('responds with 400 when no required fields are supplied', () => {
+                const idToUpdate = 2
+                return supertest(app)
+                    .patch(`/api/posts/${idToUpdate}`)
+                    .send({irrelevantField: 'foo'})
+                    .expect(400, { error: {message: `Request body must contain title and content`}})
+            })
+
+            it('responds with 204 when updating only a subset of the fields', () => {
+                const idToUpdate = 2
+                const updatePost = {
+                    title: 'Updated Post',
+                }
+                const expectedPost = {
+                    ...testPosts[idToUpdate -1],
+                    ...updatePost
+                }
+                return supertest(app)
+                    .patch(`/api/posts/${idToUpdate}`)
+                    .send({
+                        ...updatePost,
+                        fieldToIgnore: 'should not be in GET response'
+                    })
+                    .expect(204)
+                    .then(res =>
+                        supertest(app)
+                        .get(`/api/posts/${idToUpdate}`)
+                        .expect(expectedPost)    
+                    )
+            })
+        })
+    })
+});
