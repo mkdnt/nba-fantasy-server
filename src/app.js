642f2ed732eab8ae640d95cb5fb6a9b08b4f4a09
Mike Dent <mikedent85@gmail.com>
2021-02-08 12:45:27 -0500

    fixing stupid errors

--- src/app.js
+++ src/app.js
@@ -7,29 +7,15 @@ const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 const postRouter = require('./post/post-router')
 const errorHandler = require('./errorHandler');
+const validateBearerToken = require("./validateBearerToken");
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 const app = express();
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors({ origin: CLIENT_ORIGIN }));
+app.use(validateBearerToken);
 app.use('/api/posts', postRouter);
 app.use(errorHandler);
-
-
-app.get('/', (req, res) => {
-  res.send('Hello, NBA Fantasy Thing!')
-});
-
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === "production") {
-    response = { error: { message: "server error" } };
-  } else {
-    console.error("error");
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
 
 module.exports = app;
--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || "test",
+  NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://MikeDent@localhost/nbafantasy',
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://MikeDent@localhost/nbafantasytest'
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://MikeDent@localhost/nbafantasytest',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
 };
