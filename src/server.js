89e13d68161110ff66cb64baedc50a6f52e9da5d
Mike Dent <mikedent85@gmail.com>
2021-02-23 17:06:58 -0500

    again

--- src/server.js
+++ src/server.js
@@ -10,5 +10,5 @@ const db = knex({
 app.set('db', db);
 
 app.listen(PORT, () => {
-  console.log(`Express server is listening at http://localhost:${PORT}`);
+  console.log(`Express server is listening at port: ${PORT}`);
 });
