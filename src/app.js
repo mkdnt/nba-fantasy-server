2719a4f81fce59dceb43ac9a7587ac4adcb45f39
Mike Dent <mikedent85@gmail.com>
2021-02-23 16:06:27 -0500

    i dont know

--- src/app.js
+++ src/app.js
@@ -7,17 +7,15 @@ const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 const postRouter = require('./post/post-router');
 const authRouter = require('./auth/auth-router');
-const userRouter = require('./user/user-router')
-const playerRouter = require('./player/player-router')
+const userRouter = require('./user/user-router');
+const playerRouter = require('./player/player-router');
 const errorHandler = require('./errorHandler');
-//const validateBearerToken = require("./validateBearerToken");
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 const app = express();
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors({ origin: CLIENT_ORIGIN }));
-//app.use(validateBearerToken);
 app.use('/api/posts', postRouter);
 app.use('/api/auth', authRouter)
 app.use('/api/users', userRouter)
--- src/errorHandler.js
+++ src/errorHandler.js
@@ -1,14 +1,9 @@
-const { NODE_ENV } = require('./config')
+const { NODE_ENV } = require('../config')
 
-function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: {message: 'server error'} }
-    } else {
-        console.error(error)
-        response = { message: error.message, error}
-    }
-    res.status(500).json(response)
+module.exports = function errorHandler(error, req, res, next) {
+  const response = (NODE_ENV === 'production')
+    ? { error: 'Server error' }
+    : (console.error(error), { error: error.message, details: error })
+
+  res.status(500).json(response)
 }
-
-module.exports = errorHandler
